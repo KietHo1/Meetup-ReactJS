68af8395e157e331d5ebaf89a61d340722daf959
KietHo1 <kiethostartup1999@gmail.com>
2022-10-07 22:14:23 +0700

    Finish day 1

---
+++ src/components/Todo.js
@@ -0,0 +1,13 @@
+function Todo() {
+  return (
+    <div className="card">
+      <h2>Learn React</h2>
+      <div className="actions">
+        <span>A Span</span>
+        <button className="btn">Delete</button>
+      </div>
+    </div>
+  );
+}
+
+export default Todo;

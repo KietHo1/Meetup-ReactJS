3972180b89e5ec652c12825ceaefb07baf622c2b
KietHo1 <kiethostartup1999@gmail.com>
2022-10-31 06:54:17 +0700

    Main Navigation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import "./App.css";
-import Todo from "./components/Todo";
+import { Route, Switch } from "react-router-dom";
+import AllMeetupsPage from "./pages/AllMeetups";
+import NewMeetupPage from "./pages/NewMeetups";
+import FavoritesPage from "./pages/Favorites";
+import MainNavigation from "./components/layout/MainNavigation";
 function App() {
   return (
     <div>
-      <h1>My Todos</h1>
-      <Todo text="Learn React" />
-      <Todo text="Master React" />
-      <Todo text="Explore the full React course" />
+      <MainNavigation />
+      <Switch>
+        <Route path="/" exact={true}>
+          <AllMeetupsPage />
+        </Route>
+        <Route path="/new-meetup">
+          <NewMeetupPage />
+        </Route>
+        <Route path="/favorites">
+          <FavoritesPage />
+        </Route>
+      </Switch>
     </div>
   );
 }
---
+++ src/pages/NewMeetups.js
@@ -0,0 +1,5 @@
+function NewMeetupPage() {
+  return <div>Favorites Page</div>;
+}
+
+export default NewMeetupPage;

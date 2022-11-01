c6eb8815cc119406b460eccc3831039300f680f2
KietHo1 <kiethostartup1999@gmail.com>
2022-11-01 11:33:57 +0700

    Firebase + Edit NewMeetup(*/ω＼*)

--- src/pages/NewMeetups.js
+++ src/pages/NewMeetups.js
@@ -1,10 +1,13 @@
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetupPage() {
+  function addMeetupHandler(meetupData) {
+    fetch();
+  }
   return (
     <section>
       <h1>Add New Meetup</h1>
-      <NewMeetupForm />
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
     </section>
   );
 }

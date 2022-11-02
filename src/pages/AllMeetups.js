4c99f875071bce4d69ab29472e376db6d7a279c5
KietHo1 <kiethostartup1999@gmail.com>
2022-11-02 08:56:57 +0700

    Finish 💖

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -9,7 +9,7 @@ function AllMeetupsPage() {
   useEffect(() => {
     setIsLoading(true);
     fetch(
-      "https://react-getting-started-48dec-default-rtdb.firebaseio.com/meetups.json"
+      "https://react-meetup-13de6-default-rtdb.firebaseio.com//meetups.json"
     )
       .then((response) => {
         return response.json();
--- src/pages/NewMeetups.js
+++ src/pages/NewMeetups.js
@@ -7,7 +7,7 @@ function NewMeetupPage() {
 
   function addMeetupHandler(meetupData) {
     fetch(
-      "https://react-getting-started-48dec-default-rtdb.firebaseio.com/meetups.json",
+      "https://react-meetup-13de6-default-rtdb.firebaseio.com/meetups.json",
       {
         method: "POST",
         body: JSON.stringify(meetupData),
